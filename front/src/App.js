7fe93be7f03b6cfb80514b0a30f33d187c1d7ee4
Déborah <deborah.bude@gmail.com>
2023-04-14 16:38:13 +0200

    Configuration des pages utilisateurs suivant l'ID de l'url

--- front/src/App.js
+++ front/src/App.js
@@ -3,13 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./Pages/Home/index";
+import Error404 from "./Pages/404/index";
+import { sourceApi } from "./fetchAPI";
+import { sourceMock } from "./fetchMock";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<Home source={sourceApi} />} />
+				<Route path="/:id" element={<Home source={sourceApi} />} />
+				<Route path="/page-non-trouvee" element={<Error404 />} />
 			</Routes>
 			<Footer />
 		</div>
